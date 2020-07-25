95f228c28234e6c12595a19b0ee690945e0afcbc
agnieszka <agnieszka.kondek89@gmail.com>
2020-07-25 15:04:26 +0200

    dodanie slajdera w html i css + plik js

---
+++ app.js
@@ -0,0 +1,11 @@
+let person1 = {
+  name: "Filip ",
+  lastName: "Mamama"
+}
+
+let person2 = {
+  name: "agnieszka ",
+  lastName: "kondek"
+}
+console.log(person1.name + person1.lastName);
+console.log(person2.name + person2.lastName);
