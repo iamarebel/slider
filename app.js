46d279f26e12bf0f9b6e2bc791b05621e8d702f6
agnieszka <agnieszka.kondek89@gmail.com>
2020-07-26 11:46:54 +0200

    zadania funkcje

--- app.js
+++ app.js
@@ -1,8 +1,47 @@
-let arrowRight = document.querySelector("#arrow-right");
+// // let arrowRight = document.querySelector("#arrow-right");
 
-let function3 = () => {
-  console.log("strzałka kliknięta!");
+// // let function3 = () => {
+// //   console.log("strzałka kliknięta!");
+// // };
+
+// // arrowRight.addEventListener("click", function3);
+
+// // przeksztalcenie napisu w liczbe 
+// let dlugoscNapisu = (napis) => {
+//   return napis.length;
+// };
+
+// //definicja funkcji
+// let doKwadratu = (x) => {
+//   let wynik = x * x; 
+//   return wynik; 
+// };
+// //return oddaje wynik funkcji, bez niego kod nie zadziala, tylko w funkcji 
+
+// //wywolanie funkcji 
+// let dlugosc = dlugoscNapisu("abcdefgh");
+// let kwadrat = doKwadratu(dlugosc);
+
+// console.log(kwadrat);
+
+let convert = (kwota, kurs) => {
+  let wynik = kwota * kurs;
+  return wynik;
 };
 
-arrowRight.addEventListener("click", function3);
+let plnToEuro = (pln) => {
+  let przeliczenie = convert(pln, 0.23);
+  return przeliczenie;
+};
+
+let euroToUsd = (euro) => {
+  let przeliczenie = convert(euro, 1.17);
+  return przeliczenie;
+};
+
+let euro = plnToEuro(1000);
+let usd = euroToUsd(euro);
+
+console.log(euro);
+console.log(usd);
 
