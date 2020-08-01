82f8540520e5ae49f6c10d0ba44078b3c6ef6afd
agnieszka <agnieszka.kondek89@gmail.com>
2020-08-01 10:51:09 +0200

    arrow left/right functions added - if/else

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
-// let arrowRight = document.querySelector("#arrow-right");
-// let function3 = () => {
-//   console.log("strzałka kliknięta!");
-// };
-// arrowRight.addEventListener("click", function3);
-
-
-
-//gdy klikamy na 2 kropke pojawia sie 2 obrazek:
-//1. znajdzmy kropke nr 2 (dodalismy id zeby znalesc tą konkretną) i zapisujemy ja do zmiennych zeby zachowac i definiujemy funkcje mechanizmu
-// niech kroka slucha na klikniecie i wywola sie funkcja
-//2. znalezienie slajd 2 
-//3. po kliknieciu niech pojawi sie obrazek nr 2
-//3.1 znalesc obrazek 2 
-//3.2 dodac do obrazka 2 klase active
-//3.3 usunac klase active z pierwszego slajdu (remove)
+//zmienna reprezentujaca numer aktywnego slajdu
+let activeSlideNumber = 1;
 
 let dot1 = document.querySelector("#dot1");
 let dot2 = document.querySelector("#dot2");
 let dot3 = document.querySelector("#dot3");
 let dot4 = document.querySelector("#dot4");
+
+
+// deklaracja zmiennych z strzalkami , szukamy w dokumencie
+let arrowLeft = document.querySelector("#arrow-left");
+let arrowRight = document.querySelector("#arrow-right");
+
 
 let hideActiveElement = () => {
   let activeElement = document.querySelector(".active");
@@ -31,6 +23,7 @@ let hideActiveElement = () => {
 
 // wyciagamy osobna funkcje dla powtarzajacego sie kodu i wklejamy funkcje w reszte funkcji aby ise wywolywala
 let showSlide = (slideNumber) => {
+  activeSlideNumber = slideNumber;
   hideActiveElement();
   document.querySelector("#slide" + slideNumber).classList.add("active");
   document.querySelector("#dot" + slideNumber).classList.add("dot-active");
@@ -51,9 +44,37 @@ let showSlide3 = () => {
 let showSlide4 = () => {
   showSlide(4);
 }
-//niech kroka slucha na klikniecie, na co,  mechanizm  
+
+// funkcja do strzalki w lewo
+let showPreviousSlide =() => {
+  if (activeSlideNumber === 1) {
+    showSlide(4);
+  }
+  else {
+    showSlide(activeSlideNumber - 1);
+  }
+}
+
+// funkcja to strzalki w prawo 
+let showNextSlide = () => {
+  // pokaz aktywny slajd + jeden po nacisnie w prawo
+  // jesli jestesmy na ostatnim to skocz na pierwszy slajd - uzycie nistrkucji warunkowej if else 
+  if (activeSlideNumber === 4) {
+    showSlide(1);
+  }
+  else {
+     // w innym przypadku 
+    showSlide(activeSlideNumber + 1);
+  }
+}
+ 
+//niech kropka slucha na klikniecie, na co,  mechanizm  
 dot1.addEventListener("click", showSlide1);
 dot2.addEventListener("click", showSlide2);
 dot3.addEventListener("click", showSlide3);
 dot4.addEventListener("click", showSlide4);
 
+// nasluchuj na klikniecie na strzalke w lewo
+arrowLeft.addEventListener("click", showPreviousSlide);
+// nasluchuj na klikniecie na strzalke w prawo
+arrowRight.addEventListener("click", showNextSlide);
